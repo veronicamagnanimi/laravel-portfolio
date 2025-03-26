e3d73cf0147d2be8a499cdceff55ca1a354bea88
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-03-26 12:52:41 +0100

    feat: layout wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,17 @@
             </svg>
         </div>
         <h1 class="display-5 fw-bold">
-            Welcome to Laravel+Bootstrap <i class="bi bi-box"></i>
+            Welcome to my portfolio <i class="bi bi-box"></i>
         </h1>
 
         <p class="col-md-8 fs-4">
             This is a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 11.x.
             You can also use bootstrap icons out of the box.
         </p>
-        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
-    </div>
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
+        <hr>
+        <div class="social">
+            <h4>Social:</h4>
+        </div>
     </div>
 </div>
 @endsection
