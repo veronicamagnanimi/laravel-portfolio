3949442b950cfb7330da0a00d480ea7739a1e6b6
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-03-27 16:26:59 +0100

    feat: index table completed

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.projects')
+@section('title', 'All projects')
+@section('content')
+
+<div class="container mt-4">
+    <div class="table-responsive">
+        <table class="table table-striped table-hover text-center">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Description</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($projects as $project)
+                <tr>
+                    <td> {{ $project->title }}</td>
+                    <td> {{ $project->author }}</td>
+                    <td> {{ $project->description }}</td>
+                    <td>
+                        <a href="{{ route('projects.show', $project->id) }}" class="btn btn-primary btn-sm">Show Content</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
